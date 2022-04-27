93a36ae2b54cae7ff954a803d4ad837bb0193fae
Geoffrey Brunet <geoffrey.brunet@icloud.com>
2022-04-27 22:59:50 +0200

    ajout d’un test sur une liste à 2 éléments non triés avec seulement la création des buckets et la concaténation des buckets

--- bucket_sort.py
+++ bucket_sort.py
@@ -1,2 +1,25 @@
-def sort(input):
-    return input
+def sort(arr):
+
+    if len(arr) == 0 or len(arr) == 1:
+        return arr
+    else:
+        noOfBuckets = 10
+        max_ele = max(arr)
+        min_ele = min(arr)
+        rnge = (max_ele - min_ele) / noOfBuckets
+        temp = []
+        for i in range(noOfBuckets):
+            temp.append([])
+        for i in range(len(arr)):
+            diff = (arr[i] - min_ele) / rnge - int((arr[i] - min_ele) / rnge)
+            if(diff == 0 and arr[i] != min_ele):
+                temp[int((arr[i] - min_ele) / rnge) - 1].append(arr[i])
+            else:
+                temp[int((arr[i] - min_ele) / rnge)].append(arr[i])  
+        k = 0
+        for lst in temp:
+            if lst:
+                for i in lst:
+                    arr[k] = i
+                    k = k+1
+        return arr
--- test_bucket_sort.py
+++ test_bucket_sort.py
@@ -1,11 +1,19 @@
 import bucket_sort
 
+
 def test_sort_empty():
     expected = []
     input = []
     assert bucket_sort.sort(input) == expected
-    
+
+
 def testSort1Item():
     expected = [1.9]
     input = [1.9]
+    assert bucket_sort.sort(input) == expected
+
+
+def testSort2Item():
+    expected = [1.9, 18.12]
+    input = [18.12, 1.9]
     assert bucket_sort.sort(input) == expected
