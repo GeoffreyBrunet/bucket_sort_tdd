03b5c7c537165a7e228e417b52d90ad796b3221c
Geoffrey Brunet <geoffrey.brunet@icloud.com>
2022-04-28 13:07:02 +0200

    ajout d’un test sur une liste à n éléments non triés

--- test_bucket_sort.py
+++ test_bucket_sort.py
@@ -23,3 +23,9 @@ def testSort2Item():
     expected = [1.9, 18.12]
     input = [18.12, 1.9]
     assert bucket_sort.sort(input) == expected
+
+
+def testSort7Items():
+    expected = [1.7, 1.9, 2.1, 2.2, 2.3, 4.3, 8.1]
+    input = [1.9, 1.7, 4.3, 2.3, 2.2, 2.1, 8.1]
+    assert bucket_sort.sort(input) == expected
